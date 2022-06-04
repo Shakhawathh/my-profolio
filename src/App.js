215c7278bb8a7fb13feb934f4890398be16f99ff
Shakhawathh <shakhawathh55@gmail.com>
2022-06-04 23:45:38 +0600

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
-     <h1 className='font-bold'> hi there</h1>
+     <Navbar></Navbar>
     </div>
   );
 }
