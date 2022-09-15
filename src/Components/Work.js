93cbd43712b4134602c6f274ee05da95d3ab689c
Shakhawathh <shakhawathh55@gmail.com>
2022-09-15 14:41:04 +0600

    adding pic

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import WorkImg from '../assets/workImg.jpeg'
-import realEstate from '../assets/realestate.jpg'
-import bike from '../assets/Screenshot (15).png'
+import WorkImg from '../assets/1.jpg'
+import Img2 from '../assets/2.jpg'
+import Img3 from '../assets/3.jpg'
 
 
 const work = () => {
@@ -27,17 +27,17 @@ const work = () => {
               {/* Hover Effects */}
               <div className='opacity-0 group-hover:opacity-100'>
                 <span className='text-2xl font-bold text-white tracking-wider'>
-                  React JS Application
+                 Webb School
                 </span>
                 <div className='pt-8 text-center'>
-                  <a href='https://bike-parts-4299a.web.app'>
+                  <a target={'_blank'} href='https://webb-school-mission-2022.vercel.app/'>
                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                       Demo
                     </button>
                   </a>
                   <a href='/'>
                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                      Code
+                    Details
                     </button>
                   </a>
                 </div>
@@ -45,49 +45,49 @@ const work = () => {
             </div>
               {/* Grid Item */}
             <div
-              style={{ backgroundImage: `url(${realEstate})` }}
+              style={{ backgroundImage: `url(${Img3})` }}
               className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
             >
               {/* Hover Effects */}
               <div className='opacity-0 group-hover:opacity-100'>
                 <span className='text-2xl font-bold text-white tracking-wider'>
-                  React JS Application
+                 Moto Parts Inventory Management
                 </span>
                 <div className='pt-8 text-center'>
-                  <a href='https://warehouse-7020b.web.app'>
+                  <a target={'_blank'} href='https://bike-parts-4299a.web.app/'>
                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                       Demo
                     </button>
                   </a>
                   <a href='/'>
                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                      Code
+                    Details
                     </button>
                   </a>
                 </div>
               </div>
             </div>
             <div
-              style={{ backgroundImage: `url(${WorkImg})` }}
+              style={{ backgroundImage: `url(${Img2})` }}
               className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
             >
               {/* Hover Effects */}
               <div className='opacity-0 group-hover:opacity-100'>
                 <span className='text-2xl font-bold text-white tracking-wider'>
-                  React JS Application
+               Phone Inventory Management
                 </span>
                 <div className='pt-8 text-center'>
-                  <a href='/z'>
+                  <a target={'_blank'} href='https://warehouse-7020b.web.app'>
                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                       Demo
                     </button>
                   </a>
                   <a href='/'>
                     <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                      Code
+                    Details
                     </button>
                   </a>
-                </div>
+                </div>  
               </div>
             </div>
           </div>
