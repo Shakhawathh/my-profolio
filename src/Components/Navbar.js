a4348950197c17bbf5c6d50223ed696c91e4be35
Shakhawathh <shakhawathh55@gmail.com>
2023-12-07 18:47:01 +0600

    new Resume

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,111 +1,142 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import {
-    FaBars,
-    FaTimes,
-    FaGithub,
-    FaLinkedin,
-    FaFacebook,
-    FaLinkedinIn,
-} from 'react-icons/fa';
-import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
-import logo from '../assets/Shakhawath.png';
-import { Link } from 'react-scroll';
-
-;
+  FaBars,
+  FaTimes,
+  FaGithub,
+  FaLinkedin,
+  FaFacebook,
+  FaLinkedinIn,
+} from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import logo from "../assets/Shakhawath.png";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-    const [nav, setNav] = useState(false)
-    const handleClick = () => setNav(!nav)
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
 
-    return (
-        <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#180A0A] text-gray-300  '>
-            <div>
-                <img src={logo} alt='Logo Image' style={{ width: '100px' }} />
-            </div>
-            {/* Menu */}
+  return (
+    <div className="fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#180A0A] text-gray-300  ">
+      <div>
+        <img src={logo} alt="Logo Image" style={{ width: "100px" }} />
+      </div>
+      {/* Menu */}
 
-            <ul className='hidden md:flex'>
-                <li>
-                    <Link to="home" smooth={true} duration={500}>
-                        Home
-                    </Link>
-                </li>
-                <li>
-                <Link to="about" smooth={true} duration={500}>
-                        About
-                    </Link>
-                </li>
-                <li>
-                <Link to="skills" smooth={true} duration={500}>
-                        Skills
-                    </Link>
-                </li>
-                <li>
-                <Link to="projects" smooth={true} duration={500}>
-                    projects
-                    </Link>
-                </li>
-                <li>
-                <Link to="contact" smooth={true} duration={500}>
-                        Contact
-                    </Link>
-                </li>
-            </ul>
+      <ul className="hidden md:flex">
+        <li>
+          <Link to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to="projects" smooth={true} duration={500}>
+            projects
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
 
+      {/* side bar */}
+      <div onClick={handleClick} className="md:hidden z-10">
+        {!nav ? <FaBars /> : <FaTimes />}
+      </div>
 
-            {/* side bar */}
-            <div onClick={handleClick} className='md:hidden z-10'>
-                {!nav ? <FaBars /> : <FaTimes />}
-            </div>
+      {/* Mobile menu */}
 
-            {/* Mobile menu */}
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#180A0A] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="about" smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link
+            onClick={handleClick}
+            to="projects"
+            smooth={true}
+            duration={500}
+          >
+            projects
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
+      </ul>
 
-            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#180A0A] flex flex-col justify-center items-center'}>
-                <li  className='py-6 text-4xl'><Link onClick={handleClick} to="home" smooth={true} duration={500}>
-                        Home
-                    </Link></li>
-                <li className='py-6 text-4xl'><Link onClick={handleClick} to="about" smooth={true} duration={500}>
-                        About
-                    </Link></li>
-                <li className='py-6 text-4xl'><Link onClick={handleClick} to="skills" smooth={true} duration={500}>
-                        Skills
-                    </Link></li>
-                <li className='py-6 text-4xl'><Link onClick={handleClick} to="projects" smooth={true} duration={500}>
-                    projects
-                    </Link></li>
-                <li className='py-6 text-4xl'><Link onClick={handleClick} to="contact" smooth={true} duration={500}>
-                        Contact
-                    </Link></li>
-            </ul>
-
-            {/* Social icons */}
-            <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
-                <ul>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 rounded-[12px]'>
-                        <a className='flex justify-between items-center w-full text-gray-300' href='https://www.linkedin.com/in/shakhawath-hossain-shatu-a5114117b/'>
-                            Linkedin <FaLinkedin size={30} />
-                        </a>
-                    </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#17181c] rounded-[12px]'>
-                        <a className='flex justify-between items-center w-full text-gray-300' href='https://github.com/Shakhawathh?tab=repositories'>
-                            Github <FaGithub size={30} />
-                        </a>
-                    </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#1FC4Da] rounded-[12px]'>
-                        <a className='flex justify-between items-center w-full text-gray-300' href='mailto:shakhawathh55@gmail.com'>
-                            Email <HiOutlineMail size={30} />
-                        </a>
-                    </li>
-                    <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#244D61] rounded-[12px]'>
-                        <a className='flex justify-between items-center w-full text-gray-300' href='https://drive.google.com/file/d/16R_lqxYBzp2BwK_RZVDfbjWJZun-oiDJ/view?usp=sharing' target={'_blank'}>
-                            Resume <BsFillPersonLinesFill size={30} />
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
+      {/* Social icons */}
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
+        <ul>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600 rounded-[12px]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://www.linkedin.com/in/shakhawath-hossain-shatu-a5114117b/"
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#17181c] rounded-[12px]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://github.com/Shakhawathh?tab=repositories"
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#1FC4Da] rounded-[12px]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="mailto:shakhawathh55@gmail.com"
+            >
+              Email <HiOutlineMail size={30} />
+            </a>
+          </li>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#244D61] rounded-[12px]">
+            <a
+              className="flex justify-between items-center w-full text-gray-300"
+              href="https://drive.google.com/file/d/1AbOlOyPmG-siE4g-3emIutdO-RcG23R4/view?usp=drive_link"
+              target={"_blank"}
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
--- src/Components/Work.js
+++ src/Components/Work.js
@@ -1,99 +1,106 @@
-import React from 'react';
-import WorkImg from '../assets/1.jpg'
-import Img2 from '../assets/2.jpg'
-import Img3 from '../assets/3.jpg'
-
+import React from "react";
+import WorkImg from "../assets/1.jpg";
+import Img2 from "../assets/2.jpg";
+import Img3 from "../assets/3.jpg";
 
 const work = () => {
-    return (
-        <div name='projects' className='w-full  md:h-screen text-gray-300 bg-[#180A0A] py-16'>
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
-          <div className='pb-8'>
-            <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600'>
-              Work
-            </p>
-            <p className='py-6'>// Check out some of my recent work</p>
+  return (
+    <div
+      name="projects"
+      className="w-full  md:h-screen text-gray-300 bg-[#180A0A] py-16"
+    >
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
+            Work
+          </p>
+          <p className="py-6">// Check out some of my recent work</p>
+        </div>
+
+        {/* Container */}
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Grid Item */}
+
+          <div
+            style={{ backgroundImage: `url(${WorkImg})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Webb School
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  target={"_blank"}
+                  href="https://webb-school-mission-2022.vercel.app/"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Details
+                  </button>
+                </a>
+              </div>
+            </div>
           </div>
-  
-  {/* Container */}
-          <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-  
-              {/* Grid Item */}
-        
-            <div
-              style={{ backgroundImage: `url(${WorkImg})` }}
-              className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-            >
-              {/* Hover Effects */}
-              <div className='opacity-0 group-hover:opacity-100'>
-                <span className='text-2xl font-bold text-white tracking-wider'>
-                 Webb School
-                </span>
-                <div className='pt-8 text-center'>
-                  <a target={'_blank'} href='https://webb-school-mission-2022.vercel.app/'>
-                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                      Demo
-                    </button>
-                  </a>
-                  <a href='/'>
-                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+          {/* Grid Item */}
+          <div
+            style={{ backgroundImage: `url(${Img3})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Moto Parts Inventory Management
+              </span>
+              <div className="pt-8 text-center">
+                <a target={"_blank"} href="https://bike-parts-4299a.web.app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Details
-                    </button>
-                  </a>
-                </div>
+                  </button>
+                </a>
               </div>
             </div>
-              {/* Grid Item */}
-            <div
-              style={{ backgroundImage: `url(${Img3})` }}
-              className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-            >
-              {/* Hover Effects */}
-              <div className='opacity-0 group-hover:opacity-100'>
-                <span className='text-2xl font-bold text-white tracking-wider'>
-                 Moto Parts Inventory Management
-                </span>
-                <div className='pt-8 text-center'>
-                  <a target={'_blank'} href='https://bike-parts-4299a.web.app/'>
-                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                      Demo
-                    </button>
-                  </a>
-                  <a href='/'>
-                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
+          </div>
+          <div
+            style={{ backgroundImage: `url(${Img2})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                Car Rent
+              </span>
+              <div className="pt-8 text-center">
+                <a
+                  target={"_blank"}
+                  href="https://car-showcase-szh8.vercel.app/"
+                >
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Details
-                    </button>
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div
-              style={{ backgroundImage: `url(${Img2})` }}
-              className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
-            >
-              {/* Hover Effects */}
-              <div className='opacity-0 group-hover:opacity-100'>
-                <span className='text-2xl font-bold text-white tracking-wider'>
-               Phone Inventory Management
-                </span>
-                <div className='pt-8 text-center'>
-                  <a target={'_blank'} href='https://warehouse-7020b.web.app'>
-                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                      Demo
-                    </button>
-                  </a>
-                  <a href='/'>
-                    <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
-                    Details
-                    </button>
-                  </a>
-                </div>  
+                  </button>
+                </a>
               </div>
             </div>
           </div>
         </div>
       </div>
-    );
+    </div>
+  );
 };
 
 export default work;
